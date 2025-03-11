9a37b2c4257b12bfb7e89c0e91245d6d74f290cf
puck3 <shipilovk04@gmail.com>
2025-03-11 18:15:49 +0300

    add test

--- LU-decomposition/src/main.cpp
+++ LU-decomposition/src/main.cpp
@@ -4,6 +4,43 @@
 #include "LUDecomposer.hpp"
 #include "Matrix.hpp"
 
+const int eps = 1e-9;
+
+void check_solution(const Matrix& A, const std::vector<double>& b,
+                    const std::vector<double>& x) {
+  std::cout << "A * x = (";
+  for (size_t i = 0; i < A.getRows(); ++i) {
+    double res = 0;
+    for (size_t j = 0; j < A.getRows(); ++j) {
+      res += (A[i][j] * x[j]);
+    }
+    std::cout << res << " ";
+    if (res != b[i]) {
+      // throw std::runtime_error("Invalid answer");
+    }
+    res = 0;
+  }
+  std::cout << ")^T" << std::endl;
+}
+
+void check_inverse(const Matrix& A, const Matrix& InversedA) {
+  double res = 0;
+  std::cout << "A * A^(-1) = " << std::endl;
+  for (size_t i = 0; i < A.getRows(); ++i) {
+    for (size_t j = 0; j < A.getCols(); ++j) {
+      for (size_t k = 0; k < A.getRows(); ++k) {
+        res += A[i][k] * InversedA[k][j];
+      }
+      if ((i == j && abs(res - 1) > eps) || (i != j && abs(res) > eps)) {
+        throw std::runtime_error("Invalid inversed matrix");
+      }
+      std::cout << res << " ";
+      res = 0;
+    }
+    std::cout << std::endl;
+  }
+}
+
 int main() {
   try {
     size_t n;
@@ -23,11 +60,13 @@ int main() {
     std::cout << "Solution x: ";
     for (double xi : x) std::cout << xi << " ";
     std::cout << std::endl;
+    check_solution(A, b, x);
 
     std::cout << "Determinant: " << lu.determinant() << std::endl;
 
     Matrix inv = lu.inverse();
     inv.print("Inverse matrix:");
+    check_inverse(A, inv);
 
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << std::endl;
