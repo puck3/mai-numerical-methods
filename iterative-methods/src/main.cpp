82563307f704bbbbb41add68d602d31b9812fb7b
puck3 <shipilovk04@gmail.com>
2025-03-25 16:49:13 +0300

    add test

--- iterative-methods/src/main.cpp
+++ iterative-methods/src/main.cpp
@@ -7,6 +7,24 @@
 
 using namespace std;
 
+void check_solution(const vector<vector<double>>& A,
+                    const std::vector<double>& b,
+                    const std::vector<double>& x) {
+  std::cout << "A * x = (";
+  for (size_t i = 0; i < A.size(); ++i) {
+    double res = 0;
+    for (size_t j = 0; j < A.size(); ++j) {
+      res += (A[i][j] * x[j]);
+    }
+    std::cout << res << " ";
+    if (res != b[i]) {
+      // throw std::runtime_error("Wrong answer");
+    }
+    res = 0;
+  }
+  std::cout << ")^T" << std::endl;
+}
+
 int main() {
   try {
     size_t n;
@@ -29,12 +47,14 @@ int main() {
     cout << "Jacobi solution: ";
     for (double val : x_jacobi) cout << val << " ";
     cout << "\nIterations: " << jacobi.get_iterations() << endl;
+    check_solution(A, b, x_jacobi);
 
     SeidelSolver seidel(A, b, eps);
     vector<double> x_seidel = seidel.solve();
     cout << "Seidel solution: ";
     for (double val : x_seidel) cout << val << " ";
     cout << "\nIterations: " << seidel.get_iterations() << endl;
+    check_solution(A, b, x_seidel);
 
   } catch (const exception& e) {
     cerr << "Error: " << e.what() << endl;
